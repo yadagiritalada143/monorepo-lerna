213e6de1904a2b4dc4b32e45ade37d4e96ffc4ba
yadagiritalada143 <yadagiritalada@gmail.com>
2023-11-10 18:24:11 +0530

    Added client side Code for counter

---
+++ packages/virtual-dom/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+
+defineProps({
+  msg: String,
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <h1>{{ count }}</h1>
+    <button type="button" @click="count++">Click Me !</button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
